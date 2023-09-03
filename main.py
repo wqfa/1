f35b59b18474dcd68fd87a7365041965db04d9f6
XF <118357460+wqfa@users.noreply.github.com>
2023-09-03 23:50:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,19 +19,19 @@ def start(message):
  	 file_info = bot.get_file(message.document.file_id)
  	 use = bot.download_file(file_info.file_path)
  	 bot.send_message(message.chat.id, f"""<strong>انتظر…</strong>""",parse_mode="html")
- 	 cv =str("#@TeAmOn404")
+ 	 cv =str("#@Q8_10")
  	 sa = compile(use, 'dg', 'exec')
  	 sb = marshal.dumps(sa)
  	 sc = zlib.compress(sb)
  	 sd = base64.b85encode(sc)
  	 b = '#https://t.me/C15CS\nimport marshal,zlib,base64\nexec(marshal.loads(zlib.decompress(base64.b85decode(' + repr(sd) + '))))'
- 	 d = open('[@TeAmOn404].py', 'w')
+ 	 d = open('[@Q8_10].py', 'w')
  	 d.write(b+'\n'+cv)
  	 d.close()
- 	 file = {'document':open('[@TeAmOn404].py','rb')}
+ 	 file = {'document':open('[@Q8_10].py','rb')}
  	 tex = ("Done Encryption File")
  	 requests.post(f'https://api.telegram.org/bot{token}/sendDocument?chat_id={message.chat.id}&caption={tex}', files=file)
- 	 bot.send_message(message.chat.id, f"[TeAm - 404 .](t.me/C15CS)",parse_mode="markdown",disable_web_page_preview="true")
- 	 os.system(f'rm -rf [@TeAmOn404].py')
+ 	 bot.send_message(message.chat.id, f"[- منظمة النازي 🌪️ ،](t.me/C15CS)",parse_mode="markdown",disable_web_page_preview="true")
+ 	 os.system(f'rm -rf [@Q8_10].py')
   	
 bot.polling(True)
